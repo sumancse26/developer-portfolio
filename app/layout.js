120784ed73d471f2463cd4fe2d794f83de5cc92d
Md. Suman Sarkar <79307272+sumancse26@users.noreply.github.com>
2024-05-08 23:06:51 +0600

    hero section modified

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { GoogleTagManager } from '@next/third-parties/google';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/footer';
@@ -9,8 +10,9 @@ import './css/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Portfolio of Abu Said - Software Developer',
-  description: 'This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
+  title: 'Portfolio of Suman Sarkar - Software Developer',
+  description: 'This is the portfolio of Suman Sarkar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
+  image: "/favicon.ico"
 };
 
 export default function RootLayout({ children }) {
