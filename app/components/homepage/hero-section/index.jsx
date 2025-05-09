ba601867bcfd897ae65d36b86054b9507c31e829
Md. Suman Sarkar <sumancse.iu32@gmail.com>
2025-05-09 11:15:59 +0600

    feat: add routing for pages

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -58,7 +58,7 @@ function HeroSection() {
 
 					<div className="flex items-center gap-3">
 						<Link
-							href="#contact"
+							href="/contact"
 							className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
 							<button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
 								<span>Contact me</span>
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,12 +1,17 @@
-// @flow strict
+"use client";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
+	const pathname = usePathname();
+	const isActive = (path) => {
+		return pathname === path ? true : false;
+	};
 	return (
 		<nav className="bg-transparent">
 			<div className="flex items-center justify-between py-5">
 				<div className="flex flex-shrink-0 items-center">
-					<Link href="/" className=" text-[#16f2b3] text-3xl font-bold">
+					<Link href="/" className="text-[#16f2b3] text-3xl font-bold">
 						SUMAN SARKAR
 					</Link>
 				</div>
@@ -16,7 +21,7 @@ function Navbar() {
 					id="navbar-default">
 					<li>
 						<Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/about">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/about') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								ABOUT
 							</div>
 						</Link>
@@ -25,14 +30,14 @@ function Navbar() {
 						<Link
 							className="block px-4 py-2 no-underline outline-none hover:no-underline"
 							href="/experience">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/experience') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								EXPERIENCE
 							</div>
 						</Link>
 					</li>
 					<li>
 						<Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/skills">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/skills') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								SKILLS
 							</div>
 						</Link>
@@ -41,14 +46,14 @@ function Navbar() {
 						<Link
 							className="block px-4 py-2 no-underline outline-none hover:no-underline"
 							href="/education">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/education') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								EDUCATION
 							</div>
 						</Link>
 					</li>
 					<li>
 						<Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/blog') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								BLOGS
 							</div>
 						</Link>
@@ -57,7 +62,7 @@ function Navbar() {
 						<Link
 							className="block px-4 py-2 no-underline outline-none hover:no-underline"
 							href="/projects">
-							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+							<div className={isActive('/projects') ? 'text-pink-600' : 'text-sm text-white transition-colors duration-300 hover:text-pink-600'}>
 								PROJECTS
 							</div>
 						</Link>
